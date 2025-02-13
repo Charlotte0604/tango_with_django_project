f07ef93d0d58fa710a98ebe69498ded3cd96ab94
Charlotte Spence <2873913S@student.gla.ac.uk>
2025-02-13 17:38:38 +0000

    'chapter8'

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -62,7 +62,7 @@ def add_category(request):
         if form.is_valid():
             # Save the new category to the database.
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
     else:
         print(form.errors)
     
@@ -76,24 +76,23 @@ def add_page(request, category_name_slug):
 
     # You cannot add a page to a Category that does not exist...
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     
     form = PageForm()
 
     if request.method == 'POST':
         form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return redirect(reverse('rango:show_category',
+                                            kwargs={'category_name_slug':                                            category_name_slug}))
+        else:
+            print(form.errors)
 
-    if form.is_valid():
-        if category:
-            page = form.save(commit=False)
-            page.category = category
-            page.views = 0
-            page.save()
-            return redirect(reverse('rango:show_category',
-                                    kwargs={'category_name_slug':
-                                            category_name_slug}))
-    else:
-        print(form.errors)
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
